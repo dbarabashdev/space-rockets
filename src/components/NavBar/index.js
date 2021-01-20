dd788c35591bcc23261c1f9e7075e842f4562a17
dbarabashdev <dmitrij.barabashh@gmail.com>
2021-01-20 21:04:35 +0200

    fix: changed symbol to ascii code

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,7 +5,7 @@ export default function NavBar() {
   return (
     <Flex as="nav" align="center" justify="space-between" wrap="wrap" padding="6" bg="gray.800" color="white">
       <Text fontFamily="mono" letterSpacing="2px" fontWeight="bold" fontSize="lg">
-        ¡SPACE·R0CKETS!
+        ¡SPACE&#8226;R0CKETS!
       </Text>
     </Flex>
   )
