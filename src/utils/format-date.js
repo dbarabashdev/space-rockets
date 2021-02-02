b8db207872407e03ea3874debcaccdc83a96afa1
dbarabashdev <dbarabashdev@gmail.com>
2021-02-02 21:43:34 +0200

    feat: added local time in Tooltip

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -1,20 +1,17 @@
+import { DateTime } from "luxon"
+import { FORMAT_DATE, LOCAL, FORMAT_DATE_TIME, SPREAD_DATE_TIME_FORMAT } from "./constants"
+
 export function formatDate(timestamp) {
-  return new Intl.DateTimeFormat("en-US", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  }).format(new Date(timestamp))
+  return new Intl.DateTimeFormat(LOCAL, FORMAT_DATE).format(new Date(timestamp))
 }
 
 export function formatDateTime(timestamp) {
-  return new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    timeZoneName: "short",
-  }).format(new Date(timestamp))
+  return new Intl.DateTimeFormat(LOCAL, SPREAD_DATE_TIME_FORMAT).format(new Date(timestamp))
+}
+
+export function formalDateTimeLocal(timestamp) {
+  const getDateTime = DateTime.fromISO(timestamp, { locale: LOCAL, setZone: true })
+  const offsetTimeZone = getDateTime.offsetNameShort
+  const localDateTime = getDateTime.toLocaleString(FORMAT_DATE_TIME)
+  return localDateTime + " " + offsetTimeZone
 }
